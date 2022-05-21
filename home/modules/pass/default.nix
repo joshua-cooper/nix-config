87b7169b6be9b778b5ce0c85d99cd53b4df3d4dc
Josh Cooper <josh@cooper.dev>
2022-05-21 09:27:00 +0900

    Move $PASSWORD_STORE_DIR out of top level $HOME

--- home/modules/pass/default.nix
+++ home/modules/pass/default.nix
@@ -1,6 +1,8 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
+  home.sessionVariables.PASSWORD_STORE_DIR = "${config.xdg.dataHome}/password-store";
+
   programs.password-store = {
     enable = true;
     package = pkgs.pass-nodmenu.withExtensions (e: with e; [ pass-otp ]);
