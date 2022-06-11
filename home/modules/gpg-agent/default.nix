aad96af19b11a6cf2f51e148bb793dfe8e6b1064
Josh Cooper <josh@cooper.dev>
2022-06-11 21:29:29 +0900

    Add custom pinentry with bemenu/curses support

--- home/modules/gpg-agent/default.nix
+++ home/modules/gpg-agent/default.nix
@@ -1,7 +1,23 @@
+{ pkgs, ... }:
+
+let
+  pinentry = pkgs.writeShellScriptBin "pinentry" ''
+    set -eu
+
+    case "$PINENTRY_USER_DATA" in
+      bemenu) exec ${pkgs.pinentry-bemenu}/bin/pinentry-bemenu "$@" ;;
+      curses) exec ${pkgs.pinentry-curses}/bin/pinentry-curses "$@" ;;
+      *) exec ${pkgs.pinentry-curses}/bin/pinentry-curses "$@" ;;
+    esac
+  '';
+in
 {
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    pinentryFlavor = "curses";
+    pinentryFlavor = null;
+    extraConfig = ''
+      pinentry-program ${pinentry}/bin/pinentry
+    '';
   };
 }
