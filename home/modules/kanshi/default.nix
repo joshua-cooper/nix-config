5cb5419200a0c7c203afccb56a06b2006efa3e77
Josh Cooper <josh@cooper.dev>
2022-08-20 16:18:07 +0900

    Position laptop under monitor when docked

--- home/modules/kanshi/default.nix
+++ home/modules/kanshi/default.nix
@@ -11,13 +11,13 @@
 
       docked.outputs = [
         {
-          criteria = "eDP-1";
-          position = "0,160";
-        }
-        {
           criteria = "Goldstar Company Ltd LG HDR 4K 0x000071E6";
           scale = 2.0;
-          position = "1280,0";
+          position = "0,0";
+        }
+        {
+          criteria = "eDP-1";
+          position = "320,1080";
         }
       ];
     };
