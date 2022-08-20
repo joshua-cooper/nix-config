954d16b75594060fb21f47759130e42adf10031d
Josh Cooper <josh@cooper.dev>
2022-08-20 16:19:52 +0900

    Add M-[1-9] bindings to switch tmux windows

--- home/modules/tmux/default.nix
+++ home/modules/tmux/default.nix
@@ -27,6 +27,16 @@
       bind '"' split-window -c "#{pane_current_path}"
       bind % split-window -h -c "#{pane_current_path}"
       bind c new-window -c "#{pane_current_path}"
+
+      bind -n M-1 select-window -t 1
+      bind -n M-2 select-window -t 2
+      bind -n M-3 select-window -t 3
+      bind -n M-4 select-window -t 4
+      bind -n M-5 select-window -t 5
+      bind -n M-6 select-window -t 6
+      bind -n M-7 select-window -t 7
+      bind -n M-8 select-window -t 8
+      bind -n M-9 select-window -t 9
     '';
   };
 }
