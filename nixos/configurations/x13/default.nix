f1e972aa56e0c65f5dbd211cf5e9e10605ddee17
Josh Cooper <josh@cooper.dev>
2022-09-11 15:45:01 +0900

    Enable backups with sanoid and syncoid

--- nixos/configurations/x13/default.nix
+++ nixos/configurations/x13/default.nix
@@ -21,4 +21,30 @@
     sudo.enable = false;
     doas.enable = true;
   };
+
+  services.sanoid = {
+    enable = true;
+
+    datasets."x13/state" = {
+      autosnap = true;
+      autoprune = true;
+    };
+  };
+
+  services.syncoid = {
+    enable = true;
+
+    # This doesn't work since the systemd unit restrics FS access.
+    # sshKey = "/nix/passwords/backups_ed25519";
+    # TODO: put this somewhere in state
+    # Also need known_hosts for the synoid user
+    sshKey = "/etc/backups_ed25519";
+
+    commands."x13/state" = {
+      recursive = true;
+      sendOptions = "w";
+      # TODO: DNS entry for this
+      target = "backups@5.161.152.96:coffer/backups/x13";
+    };
+  };
 }
