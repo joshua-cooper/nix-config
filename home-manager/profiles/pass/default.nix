92099c469d317fe7582849fcd9c6759934252de3
Josh Cooper <josh@cooper.dev>
2022-09-11 15:44:11 +0900

    Add pass-menu overlay

--- home-manager/profiles/pass/default.nix
+++ home-manager/profiles/pass/default.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  home.sessionVariables.PASSWORD_STORE_DIR = "${config.xdg.dataHome}/password-store";
+  home = {
+    sessionVariables.PASSWORD_STORE_DIR = "${config.xdg.dataHome}/password-store";
+    # TODO: Make a module to conditionally enable this since it pulls in bemenu.
+    packages = with pkgs; [ pass-menu ];
+  };
 
   programs.password-store = {
     enable = true;
--- overlays/default.nix
+++ overlays/default.nix
@@ -6,24 +6,28 @@ let
     export BEMENU_OPTS="$(tr "\n" " " < "$XDG_CONFIG_HOME"/bemenu/config) $BEMENU_OPTS"
     exec ${command} "$@"
   '';
+
+  # TODO: make this itself an overlay to be used elsewhere?
+  pass = prev.pass-nodmenu.withExtensions (e: with e; [ pass-otp ]);
 in
 {
-  bemenu = prev.symlinkJoin {
-    name = "bemenu";
-    paths = [
-      (prev.writeShellScriptBin "bemenu" (mkBemenuWrapper "${prev.bemenu}/bin/bemenu"))
-      (prev.writeShellScriptBin "bemenu-run" (mkBemenuWrapper "${prev.bemenu}/bin/bemenu-run"))
-      prev.bemenu
-    ];
-  };
-
-  pinentry-bemenu = prev.symlinkJoin {
-    name = "pinentry-bemenu";
-    paths = [
-      (prev.writeShellScriptBin "pinentry-bemenu" (mkBemenuWrapper "${prev.pinentry-bemenu}/bin/pinentry-bemenu"))
-      prev.pinentry-bemenu
-    ];
-  };
+  # bemenu = prev.symlinkJoin {
+  #   name = "bemenu";
+  #   paths = [
+  #     (prev.writeShellScriptBin "bemenu" (mkBemenuWrapper "${prev.bemenu}/bin/bemenu"))
+  #     (prev.writeShellScriptBin "bemenu-run" (mkBemenuWrapper "${prev.bemenu}/bin/bemenu-run"))
+  #     prev.bemenu
+  #   ];
+  # };
+  #
+  # # This is broken in the pinentry wrapper script for some reason.
+  # pinentry-bemenu = prev.symlinkJoin {
+  #   name = "pinentry-bemenu";
+  #   paths = [
+  #     (prev.writeShellScriptBin "pinentry-bemenu" (mkBemenuWrapper "${prev.pinentry-bemenu}/bin/pinentry-bemenu"))
+  #     prev.pinentry-bemenu
+  #   ];
+  # };
 
   mosh = prev.mosh.overrideAttrs (old: {
     patches = (old.patches or [ ]) ++ [
@@ -34,4 +38,29 @@ in
       })
     ];
   });
+
+  pass-menu = prev.writeShellScriptBin "pass-menu" ''
+    set -eu
+
+    PASSWORD_STORE_DIR=''${PASSWORD_STORE_DIR:-$HOME/.password-store}
+
+    cd "$PASSWORD_STORE_DIR" || exit
+
+    choice=$(find . -name "*.gpg" -type f | sed -e 's/^\.\///' -e 's/\.gpg$//' | ${prev.bemenu}/bin/bemenu -i)
+
+    [ -z "$choice" ] && exit
+
+    export PINENTRY_USER_DATA=bemenu
+
+    case "$choice" in
+      *otp*) ${pass}/bin/pass otp -c "$choice" ;;
+      *) ${pass}/bin/pass -c "$choice" ;;
+    esac
+
+    # TODO: Configure the timeout in mako and use urgency here?
+    # This depends on how urgency is used by other things, but these kinds of
+    # notifications are a response to user action so shouldn't stay around
+    # long. The same can be said for brightness / volume notifications.
+    ${prev.libnotify}/bin/notify-send -t 2000 pass-menu "Copied $choice"
+  '';
 }
