484543e812f6374526342e346ff86c25a41943cb
Josh Cooper <josh@cooper.dev>
2022-09-11 16:12:37 +0900

    Add failure notification to pass-menu

--- overlays/default.nix
+++ overlays/default.nix
@@ -40,7 +40,19 @@ in
   });
 
   pass-menu = prev.writeShellScriptBin "pass-menu" ''
-    set -eu
+    set -u
+
+    # TODO: Configure the timeout in mako and use urgency here?
+    # This depends on how urgency is used by other things, but these kinds of
+    # notifications are a response to user action so shouldn't stay around
+    # long. The same can be said for brightness / volume notifications.
+    notify_copied() {
+      ${prev.libnotify}/bin/notify-send -t 2000 pass-menu "Copied ''${1:-}"
+    }
+
+    notify_failure() {
+      ${prev.libnotify}/bin/notify-send -t 5000 pass-menu "''${1:-Failed}"
+    }
 
     PASSWORD_STORE_DIR=''${PASSWORD_STORE_DIR:-$HOME/.password-store}
 
@@ -57,10 +69,15 @@ in
       *) ${pass}/bin/pass -c "$choice" ;;
     esac
 
-    # TODO: Configure the timeout in mako and use urgency here?
-    # This depends on how urgency is used by other things, but these kinds of
-    # notifications are a response to user action so shouldn't stay around
-    # long. The same can be said for brightness / volume notifications.
-    ${prev.libnotify}/bin/notify-send -t 2000 pass-menu "Copied $choice"
+    status="$?"
+
+    case "$status" in
+      0) notify_copied "$choice" ;;
+      1) notify_failure "$choice is not in the password store" ;;
+      2) notify_failure "Incorrect password" ;;
+      *) notify_failure ;;
+    esac
+
+    exit "$status"
   '';
 }
