2e586e5ce8302121499e5e3878b7839dcb79c718
Josh Cooper <josh@cooper.dev>
2022-11-04 11:24:17 +0900

    Switch to home-manager swaylock module

--- home-manager/profiles/swaylock/default.nix
+++ home-manager/profiles/swaylock/default.nix
@@ -3,10 +3,10 @@
 {
   home.packages = with pkgs; [ swaylock ];
 
-  xdg.configFile."swaylock/config".text = ''
-    image=${config.xdg.dataHome}/wallpaper
-    color=000000
-    scaling=fill
-    no-unlock-indicator
-  '';
+  programs.swaylock.settings = {
+    image = "${config.xdg.dataHome}/wallpaper";
+    color = "000000";
+    scaling = "fill";
+    no-unlock-indicator = true;
+  };
 }
