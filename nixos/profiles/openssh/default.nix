3fe08c6c5baa8ffa3a978e462badb2eaa4866f58
Josh Cooper <josh@cooper.dev>
2022-11-25 17:09:21 +0900

    Add known hosts to openssh config

--- nixos/profiles/openssh/default.nix
+++ nixos/profiles/openssh/default.nix
@@ -1,8 +1,18 @@
 {
   services.openssh = {
     enable = true;
-    ports = [ 10 ];
+
     permitRootLogin = "no";
+
     passwordAuthentication = false;
+
+    hostKeys = [{
+      path = "/etc/ssh/ssh_host_ed25519_key";
+      type = "ed25519";
+    }];
+
+    knownHosts = {
+      "x13".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFzrWwVWBZPpVP809744I0hpUncoDJ9goUBTt6JcY2AD root@x13";
+    };
   };
 }
