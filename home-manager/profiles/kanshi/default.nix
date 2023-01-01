d7e571e4c5b5418c00f78f42db88369cb0f7c824
Josh Cooper <josh@cooper.dev>
2023-01-01 16:14:44 +0900

    Update name of monitor

--- home-manager/profiles/kanshi/default.nix
+++ home-manager/profiles/kanshi/default.nix
@@ -11,7 +11,7 @@
 
       docked.outputs = [
         {
-          criteria = "Goldstar Company Ltd LG HDR 4K 0x000071E6";
+          criteria = "LG Electronics LG HDR 4K 0x000071E6";
           scale = 2.0;
           position = "0,0";
         }
