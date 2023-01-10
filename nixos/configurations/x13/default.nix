2db20b1a6948f2800592143e6c38333961ff98a1
Josh Cooper <josh@cooper.dev>
2023-01-11 00:19:07 +0900

    Move ssh known hosts config out of openssh profile
    Known hosts should be available to systems that don't run their own ssh
    daemon. For now this is moved into the x13 config but should have its
    own profile later.

--- nixos/configurations/x13/default.nix
+++ nixos/configurations/x13/default.nix
@@ -33,4 +33,9 @@
   services.zfs-backup = {
     enable = true;
   };
+
+  programs.ssh.knownHosts = {
+    "x13".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFzrWwVWBZPpVP809744I0hpUncoDJ9goUBTt6JcY2AD";
+    "zh2641b.rsync.net".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKlIcNwmx7id/XdYKZzVX2KtZQ4PAsEa9KVQ9N43L3PX";
+  };
 }
--- nixos/profiles/openssh/default.nix
+++ nixos/profiles/openssh/default.nix
@@ -10,10 +10,5 @@
       path = "/etc/ssh/ssh_host_ed25519_key";
       type = "ed25519";
     }];
-
-    knownHosts = {
-      "x13".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFzrWwVWBZPpVP809744I0hpUncoDJ9goUBTt6JcY2AD";
-      "zh2641b.rsync.net".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKlIcNwmx7id/XdYKZzVX2KtZQ4PAsEa9KVQ9N43L3PX";
-    };
   };
 }
