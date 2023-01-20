b37704e423d2707b2d439da720231b610cf4a725
Josh Cooper <josh@cooper.dev>
2023-01-20 21:08:23 +0900

    Enable eslint and prettier for svelte via null-ls

--- home-manager/profiles/neovim/plugin/null-ls.lua
+++ home-manager/profiles/neovim/plugin/null-ls.lua
@@ -4,11 +4,40 @@ local null_ls = require("null-ls")
 local sources = {}
 
 if utils.is_executable("eslint_d") then
-  sources[#sources + 1] = null_ls.builtins.diagnostics.eslint_d
+  sources[#sources + 1] = null_ls.builtins.diagnostics.eslint_d.with({
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "vue",
+      "svelte",
+    },
+  })
 end
 
 if utils.is_executable("prettierd") then
-  sources[#sources + 1] = null_ls.builtins.formatting.prettierd
+  sources[#sources + 1] = null_ls.builtins.formatting.prettierd.with({
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "vue",
+      "svelte",
+      "css",
+      "scss",
+      "less",
+      "html",
+      "json",
+      "jsonc",
+      "yaml",
+      "markdown",
+      "markdown.mdx",
+      "graphql",
+      "handlebars",
+    },
+  })
 end
 
 null_ls.setup({
