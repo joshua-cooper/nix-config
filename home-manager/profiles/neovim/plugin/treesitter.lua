475c390d2af18c7f5040c2edc90b00f6a7cd54ad
Josh Cooper <josh@cooper.dev>
2023-01-31 16:19:29 +0900

    Disable treesitter on large files

--- home-manager/profiles/neovim/plugin/treesitter.lua
+++ home-manager/profiles/neovim/plugin/treesitter.lua
@@ -1,6 +1,13 @@
 require("nvim-treesitter.configs").setup({
   highlight = {
     enable = true,
+    disable = function(lang, buf)
+      local max_filesize = 500 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
   },
   indent = {
     enable = false,
