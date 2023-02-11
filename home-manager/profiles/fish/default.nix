4ab10537bd0942e8480d4e2113d1cd7a38d31336
Josh Cooper <josh@cooper.dev>
2023-02-11 22:26:42 +0900

    Clean up fish prompt
    - Remove config specific to SSH sessions
    - Use double quotes where practical
    - Format error status codes like this: [127]
    - Add spacing

--- home-manager/profiles/fish/default.nix
+++ home-manager/profiles/fish/default.nix
@@ -2,28 +2,25 @@
   programs.fish = {
     enable = true;
 
-    interactiveShellInit = ''
-      set fish_color_host_remote "$fish_color_host"
-    '';
-
     functions = {
       fish_prompt = {
         body = ''
-          if test -n "$SSH_TTY"
-            echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
-          end
-
-          echo -n (set_color blue)(prompt_pwd)' '
+          echo -n "$(set_color blue)$(prompt_pwd)"
 
           if set -q DEVBOX
-            echo -n (set_color white)"(devbox) "
+            echo -n " $(set_color white)(devbox)"
           end
 
           set_color -o
+
           if fish_is_root_user
-            echo -n (set_color red)'# '
+            echo -n " $(set_color red)# "
           end
-          echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+
+          echo -n " $(set_color red)❯$(set_color yellow)❯$(set_color green)❯"
+
+          echo -n " "
+
           set_color normal
         '';
       };
@@ -31,8 +28,9 @@
       fish_right_prompt = {
         body = ''
           set -l cmd_status $status
+
           if test $cmd_status -ne 0
-            echo -n (set_color red)"✘ $cmd_status"
+            echo -n (set_color red)"[$cmd_status]"
           end
 
           if not command -sq git
@@ -72,6 +70,7 @@
           # Get the stash status.
           # (git stash list) is very slow. => Avoid using it.
           set -l status_stashed 0
+
           if test -f "$git_dir/refs/stash"
             set status_stashed 1
           else if test -r "$git_dir/commondir"
@@ -116,29 +115,35 @@
           #  B  |    |    |    | m  | r  | m  | u  |    |    |    |
           #  ?  |    |    |    | m  | r  | m  | u  |    |    | t  |
           #  _  |    |    | d  | m  | r  | m  | u  |    |    |    |
-          set -l porcelain_status (command git status --porcelain | string sub -l2)
 
+          set -l porcelain_status (command git status --porcelain | string sub -l2)
           set -l status_added 0
+          set -l status_deleted 0
+          set -l status_modified 0
+          set -l status_renamed 0
+          set -l status_unmerged 0
+          set -l status_untracked 0
+
           if string match -qr '[ACDMT][ MT]|[ACMT]D' $porcelain_status
             set status_added 1
           end
-          set -l status_deleted 0
+
           if string match -qr '[ ACMRT]D' $porcelain_status
             set status_deleted 1
           end
-          set -l status_modified 0
+
           if string match -qr '[MT]$' $porcelain_status
             set status_modified 1
           end
-          set -l status_renamed 0
+
           if string match -qe R $porcelain_status
             set status_renamed 1
           end
-          set -l status_unmerged 0
+
           if string match -qr 'AA|DD|U' $porcelain_status
             set status_unmerged 1
           end
-          set -l status_untracked 0
+
           if string match -qe '\?\?' $porcelain_status
             set status_untracked 1
           end
@@ -153,39 +158,50 @@
             end
             echo -n " $branch"
           end
+
           if test -n "$commit"
-            echo -n ' '(set_color yellow)"$commit"
+            echo -n " $(set_color yellow)$commit"
           end
+
           if test -n "$action"
             set_color normal
-            echo -n (set_color white)':'(set_color -o brred)"$action"
+            echo -n "$(set_color white):$(set_color -o brred)$action"
           end
+
           if test $status_ahead -ne 0
-            echo -n ' '(set_color brmagenta)'⬆'
+            echo -n " $(set_color brmagenta)⬆"
           end
+
           if test $status_behind -ne 0
-            echo -n ' '(set_color brmagenta)'⬇'
+            echo -n " $(set_color brmagenta)⬇"
           end
+
           if test $status_stashed -ne 0
-            echo -n ' '(set_color cyan)'✭'
+            echo -n " $(set_color cyan)✭"
           end
+
           if test $status_added -ne 0
-            echo -n ' '(set_color green)'✚'
+            echo -n " $(set_color green)✚"
           end
+
           if test $status_deleted -ne 0
-            echo -n ' '(set_color red)'✖'
+            echo -n " $(set_color red)✖"
           end
+
           if test $status_modified -ne 0
-            echo -n ' '(set_color blue)'✱'
+            echo -n " $(set_color blue)✱"
           end
+
           if test $status_renamed -ne 0
-            echo -n ' '(set_color magenta)'➜'
+            echo -n " $(set_color magenta)➜"
           end
+
           if test $status_unmerged -ne 0
-            echo -n ' '(set_color yellow)'═'
+            echo -n " $(set_color yellow)═"
           end
+
           if test $status_untracked -ne 0
-            echo -n ' '(set_color white)'◼'
+            echo -n " $(set_color white)◼"
           end
 
           set_color normal
