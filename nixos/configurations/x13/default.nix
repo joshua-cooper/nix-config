fc8d3a4d6606cb543702a5750ecef995cc814d33
Josh Cooper <josh@cooper.dev>
2023-02-11 20:45:45 +0900

    Remove manual network interface config

--- nixos/configurations/x13/default.nix
+++ nixos/configurations/x13/default.nix
@@ -59,17 +59,9 @@
 
   networking = {
     hostName = "x13";
-
     hostId = "c69bb763";
-
     nameservers = [ "1.1.1.1#one.one.one.one" ];
-
     useNetworkd = true;
-
-    interfaces = {
-      enp0s31f6.useDHCP = true;
-      wlan0.useDHCP = true;
-    };
   };
 
   systemd.network.wait-online.anyInterface = true;
@@ -134,4 +126,6 @@
 
     home.stateVersion = "22.05";
   };
+
+  virtualisation.podman.enable = true;
 }
