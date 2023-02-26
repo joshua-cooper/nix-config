161a4e9a3dd52d05773166e5471f83568eb1d80f
Josh Cooper <josh@cooper.dev>
2023-02-26 21:21:33 +0900

    Pin bemenu to a specific commit

--- overlays/bemenu/default.nix
+++ overlays/bemenu/default.nix
@@ -3,12 +3,12 @@ final: prev:
 {
   bemenu = prev.bemenu.overrideAttrs (old: rec {
     pname = "bemenu";
-    version = "master";
+    version = "0c0107e8cba23df06252bb58724f8a01e9fc3700";
     src = prev.fetchFromGitHub {
       owner = "Cloudef";
       repo = pname;
       rev = version;
-      sha256 = "sha256-JtMvsHXdY3Khu/TRPM0cHfrWqjbrJAPzgYk4hX6WMsQ=";
+      sha256 = "sha256-Mco0GxBbpy4oKzkZtisEFVCuScwr/ajkOyewjf6HLh4=";
     };
   });
 }
