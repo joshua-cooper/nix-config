7c159d4c2517e23c2fceee5cab4b12c7bd81e27e
Josh Cooper <josh@cooper.dev>
2023-03-05 20:58:43 +0900

    Remove git status from fish prompt
    This info is now shown in the tmux status bar so having it in fish is
    redundant. This implementation was noticeably slow anyway, so removing
    it will give a good performance boost.

--- home-manager/profiles/fish/default.nix
+++ home-manager/profiles/fish/default.nix
@@ -21,6 +21,8 @@ in
     functions = {
       fish_prompt = {
         body = ''
+          set -l cmd_status $status
+
           if set -q DEVBOX
             echo -n "📦 "
           end
@@ -30,65 +32,16 @@ in
           set_color -o
 
           if fish_is_root_user
-            echo -n " $(set_color red)# "
+            echo -n " $(set_color red)#"
+          end
+
+          if test $cmd_status -ne 0
+            echo -n " $(set_color red)$cmd_status"
           end
 
           echo -n " $(set_color red)❯$(set_color yellow)❯$(set_color green)❯"
 
           echo -n " "
-
-          set_color normal
-        '';
-      };
-
-      fish_right_prompt = {
-        body = ''
-          set -l cmd_status $status
-
-          if test $cmd_status -ne 0
-            echo -n (set_color red)"[$cmd_status]"
-          end
-
-          if not command -sq git
-            set_color normal
-            return
-          end
-
-          if not set -l git_dir (command git rev-parse --git-dir 2>/dev/null)
-            set_color normal
-            return
-          end
-
-          set -l commit ""
-          if set -l action (fish_print_git_action "$git_dir")
-            set commit (command git rev-parse HEAD 2> /dev/null | string sub -l 7)
-          end
-
-          set -l branch_detached 0
-          if not set -l branch (command git symbolic-ref --short HEAD 2>/dev/null)
-            set branch_detached 1
-            set branch (command git describe --contains --all HEAD 2>/dev/null)
-          end
-
-          set_color -o
-
-          if test -n "$branch"
-            if test $branch_detached -ne 0
-              set_color brmagenta
-            else
-              set_color green
-            end
-            echo -n " $branch"
-          end
-
-          if test -n "$commit"
-            echo -n " $(set_color yellow)$commit"
-          end
-
-          if test -n "$action"
-            set_color normal
-            echo -n "$(set_color white):$(set_color -o brred)$action"
-          end
 
           set_color normal
         '';
