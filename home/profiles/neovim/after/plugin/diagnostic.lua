fe09e27aed9c3a5280517308f6ff7eda6d3c665b
Josh Cooper <josh@cooper.dev>
2023-07-12 08:06:48 +0400

    Add diagnostic virtual text

--- home/profiles/neovim/after/plugin/diagnostic.lua
+++ home/profiles/neovim/after/plugin/diagnostic.lua
@@ -2,7 +2,7 @@ vim.diagnostic.config({
   signs = false,
   underline = true,
   update_in_insert = false,
-  virtual_text = false,
+  virtual_text = true,
 })
 
 vim.keymap.set("n", "<c-k>", vim.diagnostic.open_float, { desc = "Show diagnostic" })
